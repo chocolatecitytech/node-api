468bf747da003ae48f00052acff2a6da8ad010be
derick bonsu <chocolatecitytech.com>
2020-11-07 23:57:16 -0500

    added users api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
- 
+const users = require('./users.json');
 app.get('/', express.static('public'))
-app.listen(port, () => console.log(`Recipes  app is listening at http://localhost:${port}`))
+app.get('/api/users', (req, res) =>{
+    res.json(users);
+})
+app.listen(port, () => console.log(`Users  app is listening at http://localhost:${port}`))
