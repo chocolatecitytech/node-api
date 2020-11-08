33ab42c8c4e7ef33790ebb84705587a5a3a6fe25
chocolatecitytech <68204094+chocolatecitytech@users.noreply.github.com>
2020-11-08 00:15:53 -0500

    added products api and changed express server (#1)
    Co-authored-by: derick bonsu <chocolatecitytech.com>

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const users = require('./users.json');
+const products = require('./products.json');
 app.get('/', express.static('public'))
 app.get('/api/users', (req, res) =>{
     res.json(users);
+})
+app.get('/api/products', (req, res) =>{
+    res.json(products);
 })
 app.listen(port, () => console.log(`Users  app is listening at http://localhost:${port}`))
